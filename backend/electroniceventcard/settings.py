e4a9b4ccfead295f912b85ae6281c26dc277c5d9
Jacobgijah <jacobgijjah@gmail.com>
2024-11-20 16:36:35 +0300

    register projects app event and card app

--- backend/electroniceventcard/settings.py
+++ backend/electroniceventcard/settings.py
@@ -22,9 +22,12 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # custom_app
+    'card',
+    'event',
 ]
 
 MIDDLEWARE = [
