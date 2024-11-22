84964cb8cc50c4456d98e000573cc7dcda9e410c
Jacobgijah <jacobgijjah@gmail.com>
2024-11-22 12:02:30 +0300

    added admin display

--- backend/eventapp/admin.py
+++ backend/eventapp/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Event)
+class EventAdmin(admin.ModelAdmin):
+  list_display = ['name', 'date', 'location', 'description', 'organizer']
+  list_per_page = 10
+
+
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+  list_display = ['name', 'phone_number', 'role']
+  list_per_page = 10
+
+
+@admin.register(models.Coordinator)
+class CoordinatorAdmin(admin.ModelAdmin):
+  list_display = ['event', 'user']
+
+
+@admin.register(models.Contribution)
+class ContributionAdmin(admin.ModelAdmin):
+  list_display = ['user', 'event', 'class_type', 'amount', 'amount_paid', 'payment_method', 'status', 'contributed_at']
+  list_editable = ['amount_paid']
+  list_per_page = 10
+
+
+@admin.register(models.Invitation)
+class InvitationAdmin(admin.ModelAdmin):
+  list_display = ['guest_name', 'event', 'contact_info', 'status']
+  list_per_page = 10
+
+@admin.register(models.Notification)
+class NotificationAdmin(admin.ModelAdmin):
+  list_display = ['user', 'event', 'message', 'type', 'sent_at']
