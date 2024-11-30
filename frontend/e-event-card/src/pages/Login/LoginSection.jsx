d8e056f2a878964d527f4314bb6e9f4c62dcd966
Jacobgijah <jacobgijjah@gmail.com>
2024-11-30 23:11:59 +0300

    frontend Login page complete

---
+++ frontend/e-event-card/src/pages/Login/LoginSection.jsx
@@ -0,0 +1,190 @@
+import {
+  Image,
+  Button,
+  Input,
+  Box,
+  Flex,
+  Text,
+  Container,
+} from "@chakra-ui/react";
+import React from "react";
+
+export default function LoginSection() {
+  return (
+    <>
+      {/* login section */}
+      <Box>
+        <Box>
+          <Flex
+            bg="gray.500"
+            justifyContent="center"
+            py={{ md: "120px", base: "16px", sm: "20px" }}
+          >
+            <Container
+              mt="8px"
+              display="flex"
+              justifyContent="center"
+              px={{ md: "0px", base: "20px" }}
+              w={{ md: "80%", base: "100%" }}
+            >
+              <Flex
+                gap={{ base: "31px", sm: "62px" }}
+                w="100%"
+                flexDirection="column"
+              >
+                <Text size="text4xl" color="gray.50" lineHeight="61px">
+                  <>
+                    Login Now To Start <br />
+                    Your Event Planning
+                  </>
+                </Text>
+                <Flex
+                  mb="14px"
+                  gap="20px"
+                  flexDirection="column"
+                  alignItems="flex-start"
+                >
+                  <Text size="textmd" color="gray.50">
+                    Lorem ipsum dolor sit amet,{" "}
+                  </Text>
+                  <Text
+                    size="textxs"
+                    color="white.a700"
+                    w={{ md: "60%", base: "100%" }}
+                    lineHeight="30px"
+                  >
+                    But I must explain to you how all this mistaken idea of
+                    denouncing pleasure and praising pain was born and I will
+                    give you a complete account account of the system, and
+                    expound th of the system,
+                  </Text>
+                </Flex>
+              </Flex>
+            </Container>
+          </Flex>
+          <Flex
+            mt="-446px"
+            position="relative"
+            alignItems="center"
+            mx={{ md: "80px", base: "0px" }}
+            flexDirection={{ md: "row-reverse", base: "column" }} // Switched to "row-reverse"
+          >
+            {/* Registration Form Section */}
+            <Flex
+              ml={{ md: "-58px", base: "0px" }}
+              gap="20px" // Reduced gap between elements
+              position="relative"
+              bg="white.a700"
+              boxShadow="xs"
+              w={{ md: "40%", base: "100%" }}
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="center"
+              px={{ md: "40px", base: "16px" }} // Reduced horizontal padding
+              py={{ md: "210px", base: "12px", sm: "16px" }} // Reduced vertical padding
+            >
+              <Text>Fill Your Inputs</Text>
+              <Flex
+                gap="24px" // Reduced gap between form fields
+                w={{ md: "92%", base: "100%" }}
+                flexDirection="column"
+              >
+                <Flex gap="12px" flexDirection="column" alignItems="flex-start">
+                  <Text size="textmd">Phone</Text>
+                  <Input
+                    placeholder={+255}
+                    fontFamily="Stigfier"
+                    alignSelf="stretch"
+                  />
+                </Flex>
+                <Flex gap="12px" flexDirection="column" alignItems="flex-start">
+                  <Text size="textmd">Password</Text>
+                  <Input
+                    placeholder={`********`}
+                    type="password"
+                    fontFamily="Stigfier"
+                    alignSelf="stretch"
+                  />
+                </Flex>
+                <Button
+                  size="sm" // Reduced button size
+                  color="gray.50"
+                  fontFamily="Stigfier"
+                  textTransform="capitalize"
+                  minw="116px"
+                  borderRadius="5px"
+                >
+                  Login
+                </Button>
+              </Flex>
+            </Flex>
+
+            {/* Content Section */}
+            <Flex
+              mb="0px"
+              gap="2px"
+              zIndex={1}
+              alignSelf="flex-end"
+              w={{ md: "60%", base: "100%" }}
+              position="relative"
+              flexDirection="column"
+              px={{ md: 0, base: "20px" }}
+            >
+              <Text size="text4xl" lineHeight="60px">
+                Celebrating Love, One Frame at a Time
+              </Text>
+              <Text
+                size="textxs"
+                color="black.900_cc"
+                w={{ md: "74%", base: "100%" }}
+                lineHeight="30px"
+              >
+                <>
+                  But I must explain to you how all this mistaken idea of
+                  denouncing pleasure and praising pain <br />
+                  was born and I will give you a complete account of the system,
+                  and expound th
+                </>
+              </Text>
+              <Flex flexDirection={{ md: "row", base: "column" }}>
+                <Button
+                  size="2xl"
+                  fontFamily="Stigfier"
+                  minw="288px"
+                  borderRadius="34px"
+                >
+                  Explore Now
+                </Button>
+                <Flex
+                  gap="34px"
+                  flex={1}
+                  alignItems="center"
+                  px={{ md: "56px", base: "16px", sm: "20px" }}
+                  alignSelf={{ md: "auto", base: "stretch" }}
+                  flexDirection={{ base: "column", sm: "row" }}
+                >
+                  <Image
+                    src="images/img_frame_9.svg"
+                    alt="Frame Image"
+                    h="68px"
+                    w={{ base: "100%", sm: "46%" }}
+                    fit="contain"
+                  />
+                  <Flex
+                    flex={1}
+                    flexDirection="column"
+                    alignItems="flex-start"
+                    alignSelf={{ base: "stretch", sm: "auto" }}
+                  >
+                    <Text color="gray.500">230+</Text>
+                    <Text size="textxs">Wedding Theme</Text>
+                  </Flex>
+                </Flex>
+              </Flex>
+            </Flex>
+          </Flex>
+        </Box>
+      </Box>
+    </>
+  );
+}
--- frontend/e-event-card/src/pages/Login/index.jsx
+++ frontend/e-event-card/src/pages/Login/index.jsx
@@ -1,9 +1,63 @@
+import { Helmet } from "react-helmet";
+import Header from "components/Header";
+import Footer from "components/Footer";
+import HeroSection from "pages/Landing/HeroSection";
+import LanguageSelectionSection from "components/LanguageSelectionSection";
+import LoginSection from "./LoginSection";
+import { Box, Image, Flex } from "@chakra-ui/react";
 import React from "react";
+
 
 export default function LoginPage() {
   return (
     <>
-      
+      <Helmet>
+        <title>Event Login - Start Planning Your Wedding</title>
+        <meta 
+          name="description"
+          content="Login now to begain planning your dream wedding. Experience premium services, competitive prices, and make your love story unforgettable."
+        />
+      </Helmet>
+      <Box 
+        overflowX="scroll" 
+        bg="gray.50" 
+      >
+        <Box>
+          {/* language section */}
+          <LanguageSelectionSection />
+
+          {/* header section */}
+          <Header />
+          <Image 
+            src="images/img_frame_427321538.png" 
+            alt="Image" 
+            h="10px" 
+            mt="26px"
+            w="100%" 
+            fit="cover" 
+          />
+
+          <Flex
+            mt="10px"
+            gap={{ md: "76px", base: "38px", sm: "57px" }}
+            flexDirection="column"
+          >
+            {/* login section */}
+            <LoginSection />
+          </Flex>
+
+          {/* hero section */}
+          <HeroSection />
+          <Image src="images/img_frame_427321538.png" 
+            alt="Image" 
+            h="10px"
+            mt="26px"
+            w="100%"
+            fit="cover"
+          />
+        </Box>
+        <Footer />
+      </Box>
     </>
   );
 }
